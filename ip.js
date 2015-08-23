8b5bd8935b2fe6029a268c9ca9d3df110fa22524
Axel Orn <axel@fenrir.absalon.is>
2015-08-23 18:04:44 +0000

    Fix datetime representation

--- ip.js
+++ ip.js
@@ -1,10 +1,21 @@
 var http = require('http');
 
+function pad(num) {
+    return ('0' + num).slice(-2);
+}
+
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    var date = new Date()
-    var timestamp = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + date.getDay() + "." + date.getMonth() + "." + date.getFullYear()
+    var date = new Date();
+    var hours = pad(date.getHours());
+    var minutes = pad(date.getMinutes());
+    var seconds = pad(date.getSeconds());
+    var day = pad(date.getDate());
+    var month = pad(date.getMonth());
+    var year = date.getFullYear();
+
+    var timestamp = hours + ":" + minutes + ":" + seconds + " " + day + "." + month + "." + year;
     console.log(timestamp + " - returning ip " + ip);
     res.end(ip + "\n");
 });
